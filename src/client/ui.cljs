ca663ab33ea1f114a7ff09e0c5537cbc2f2e0e0e
Keigo Suzukawa <k.suzukawa@gmail.com>
2017-04-16 14:14:33 +0100

    Minor ui tweaks

--- src/client/ui.cljs
+++ src/client/ui.cljs
@@ -3,14 +3,16 @@
             [untangled.client.core :as uc]
             [om.dom :as dom]))
 
-(defui ^:once Root
+(defui ^:once App
   static uc/InitialAppState
-  (initial-state [this params] {:ui/react-key "ROOT"
+  (initial-state [this params] {:ui/react-key "App"
                                 :some-data 42})
   static om/IQuery
   (query [this] [:ui/react-key :some-data])
   Object
   (render [this]
           (let [{:keys [ui/react-key some-data]} (om/props this)]
-            (dom/div #js {:key react-key}
+            (dom/div #js {:key react-key
+                          :className "heading"}
+
                      (str "Hello world: " some-data)))))
