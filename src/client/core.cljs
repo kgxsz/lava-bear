c1d1d843dc5d5901b59a9bdc9331a37084e78c16
Keigo Suzukawa <k.suzukawa@gmail.com>
2017-04-16 14:14:18 +0100

    No longer needed

--- src/client/core.cljs
+++
@@ -1,6 +0,0 @@
-(ns client.core
-  (:require [untangled.client.core :as uc]))
-
-;; TODO - maybe this should be the system here, app is a system component, maybe it'll be called ui
-
-(defonce app (atom (uc/new-untangled-client)))
