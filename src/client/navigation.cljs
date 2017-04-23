8653d6420687e8dbd86285e81b626e393bbdac58
Keigo Suzukawa <k.suzukawa@gmail.com>
2017-04-23 15:02:47 +0100

    Simplified url situation

--- src/client/navigation.cljs
+++ src/client/navigation.cljs
@@ -8,20 +8,19 @@
 
 (defn navigate [{:keys [config browser]} {:keys [handler query route-params url replace?]}]
   (let [{:keys [client-routes]} config
-        {:keys [!navigation]} browser]
-    (if url
-      (pushy/set-token! @!navigation url)
-      (let [{:keys [client-routes]} config
-            {:keys [!navigation]} browser
-            query-string (when-not (s/blank? (url/map->query query)) (str "?" (url/map->query query)))
-            route-params (-> route-params vec flatten)
-            path (apply bidi/path-for client-routes handler route-params)]
-        ((if replace? pushy/replace-token! pushy/set-token!) @!navigation (str path query-string))))))
+        {:keys [!navigation]} browser
+        query-string (when-not (s/blank? (url/map->query query)) (str "?" (url/map->query query)))
+        route-params (-> route-params vec flatten)
+        path (when-not url (apply bidi/path-for client-routes handler route-params))]
+    ((if replace? pushy/replace-token! pushy/set-token!) @!navigation (or url (str path query-string)))))
 
 (defn start-navigation [reconciler !navigation client-routes]
   (reset! !navigation (pushy/pushy
-                       (fn [location]
+                       (fn [{:keys [handler] :as location}]
+
                          ;; TODO - decompose the path here please
+
+
                          (js/console.warn "pushy detected a change")
                          (om/transact! reconciler `[(app/navigate ~location) :handler]))
                        (partial bidi/match-route client-routes)))
