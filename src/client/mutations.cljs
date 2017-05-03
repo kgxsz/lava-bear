1b8c05dcfd545f485081123cf6d032f67eae9a6d
Keigo Suzukawa <k.suzukawa@gmail.com>
2017-05-03 20:08:22 +0100

    Bring back a mutation for updating the list

--- src/client/mutations.cljs
+++ src/client/mutations.cljs
@@ -4,20 +4,20 @@
             [untangled.client.core :as uc]
             [untangled.client.mutations :as m]))
 
-#_(defmethod m/mutate 'app/add-item [{:keys [state ref]} k {:keys [label]}]
+(defmethod m/mutate 'app/add-item [{:keys [state ref]} k {:keys [label]}]
   {:action (fn []
              (let [list-path (conj ref :items)
-                   new-item (uc/initial-state ui/Item {:label label})
+                   new-item (uc/initial-state ui/Item {:id (rand-int 100) :label label})
                    item-ident (om/ident ui/Item new-item)]
                ;; place the item in the db table items
                (swap! state assoc-in item-ident new-item)
                ;; tack on the ident of the item in the list
                (uc/integrate-ident! state item-ident :append list-path)))})
 
-#_(defmethod m/mutate 'app/choose-tab [{:keys [state]} _ {:keys [tab]}]
-  ;; secretary calls this transaction for routing
+(defmethod m/mutate 'fetch/items-loaded [{:keys [state]} _ _]
   {:action (fn []
-             (swap! state assoc :tabs [tab 1]))})
+             (js/console.warn "items loaded yo!")
+             (js/console.warn (pr-str @state)))})
 
 (defmethod m/mutate 'app/navigate [{:keys [state]} _ {:keys [page handler route-params query-params]}]
   {:action (fn []
