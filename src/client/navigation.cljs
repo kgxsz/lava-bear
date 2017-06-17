5cb1ef65a348f3a67995cf740c29d52a8b143541
Keigo Suzukawa <k.suzukawa@gmail.com>
2017-06-17 13:22:18 +0100

    Separate the internal and external navigation functions

--- src/client/navigation.cljs
+++ src/client/navigation.cljs
@@ -7,15 +7,17 @@
             [pushy.core :as p]
             [taoensso.timbre :as log]))
 
-(defn navigate [component {:keys [handler query-params route-params url replace?]}]
+(defn navigate-external [_ {:keys [url query-params]}]
+  (let [query-string (when-not (s/blank? (url/map->query query-params)) (str "?" (url/map->query query-params)))]
+    (set! js/window.location (str url query-string))))
+
+(defn navigate-internal [component {:keys [handler query-params route-params replace?]}]
   (let [{{:keys [client-routes]} :config {:keys [navigation]} :browser} (om/shared component)
         query-string (when-not (s/blank? (url/map->query query-params)) (str "?" (url/map->query query-params)))
         route-params (-> route-params vec flatten)
-        path (when-not url (apply b/path-for client-routes handler route-params))]
-    (if url
-      ;; TODO - there's gotta be a way to do this with fucking pushy
-      (set! js/window.location (str url query-string))
-      ((if replace? p/replace-token! p/set-token!) @navigation (str path query-string)))))
+        replace-or-set (if replace? p/replace-token! p/set-token!)
+        path (str (apply b/path-for client-routes handler route-params) query-string)]
+    (replace-or-set @navigation path)))
 
 (defn start-navigation [reconciler navigation client-routes]
   (reset! navigation (p/pushy
